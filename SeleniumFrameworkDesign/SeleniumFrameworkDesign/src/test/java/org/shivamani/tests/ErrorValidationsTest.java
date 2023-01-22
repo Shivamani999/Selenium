b30358db444dd3ac514e46ba6ffcbf2e9c63f299
letsbreakcode <jaddusuri00@gmail.com>
2023-01-22 11:08:04 +0530

    The Complete Selenium POM Framework

---
+++ SeleniumFrameworkDesign/SeleniumFrameworkDesign/src/test/java/org/shivamani/tests/ErrorValidationsTest.java
@@ -0,0 +1,49 @@
+package org.shivamani.tests;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.shivamani.TestComponents.BaseTest;
+import org.shivamani.TestComponents.Retry;
+import org.shivamani.pageobjects.CartPage;
+import org.shivamani.pageobjects.CheckoutPage;
+import org.shivamani.pageobjects.ConfirmationPage;
+import org.shivamani.pageobjects.ProductCatalogue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
